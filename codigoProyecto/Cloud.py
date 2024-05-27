9e662f1b038276cf0bbe5739ace86d1223e3fc6e
JoseRodriguez <93455373+Kose117@users.noreply.github.com>
2024-05-27 12:11:51 -0500

    Fix: correccion cloud

--- codigoProyecto/Cloud.py
+++ codigoProyecto/Cloud.py
@@ -1,3 +1,19 @@
+import zmq
+from Alerta import Alerta
+
 class Cloud:
-    def funcion1():
-        pass
+    def __init__(self):
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.SUB)
+        self.socket.bind("tcp://*:5555")  
+        self.socket.setsockopt_string(zmq.SUBSCRIBE, "")  
+
+    def receive_alerts(self):
+        while True:
+            alerta_data = self.socket.recv_json()  
+            alerta = Alerta(**alerta_data)  
+            self.write_to_file(alerta)
+
+    def write_to_file(self, alerta):
+        with open("Alertas.txt", "a") as file:
+            file.write(f"Fecha: {alerta.fecha}, Origen del sensor: {alerta.origen_sensor}, Tipo de alerta: {alerta.tipo_alerta}\n")
